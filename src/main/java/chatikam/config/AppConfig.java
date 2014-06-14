7879cd2b8da6291dda34384d873985e07b485746
Toshiaki Maki <makingx@gmail.com>
2014-06-14 18:01:52 +0900

    remove unused dependencies

--- src/main/java/chatikam/config/AppConfig.java
+++ src/main/java/chatikam/config/AppConfig.java
@@ -1,15 +1,5 @@
 package chatikam.config;
 
-import javax.sql.DataSource;
-
-import net.sf.log4jdbc.Log4jdbcProxyDataSource;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
